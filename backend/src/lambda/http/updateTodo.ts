a76b1d77350a9430d52ed55e51838322cfc6d3da
Rohit Yadav <rohity2304@gmail.com>
2020-05-30 23:13:04 +0530

    Implemented update and delete todo lambdas

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,13 +1,40 @@
 import 'source-map-support/register'
-
-import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult
+} from 'aws-lambda'
+import * as AWS from 'aws-sdk'
+import { getUserId } from '../utils'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const documentClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODOS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
+  const updatedItem = {
+    todoId: todoId,
+    userId: getUserId(event),
+    ...updatedTodo
+  }
+
+  await documentClient
+    .put({ TableName: todosTable, Item: updatedItem })
+    .promise()
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      updatedItem
+    })
+  }
 }
